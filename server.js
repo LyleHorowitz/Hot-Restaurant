03131b4fbc987bebfa8cec5b694f1eed3422697a
Anita Brument <Anita@Anitas-MacBook-Pro.local>
2017-08-23 11:09:09 -0700

    server

--- server.js
+++ server.js
@@ -29,6 +29,18 @@ var reservation = [{
   unique_id: 1234
 }];
 
+var waitlist = [{
+  name: "name",
+  phone_number: "555-5555",
+  email: "email",
+  unique_id: 123
+},{
+  name: "name2",
+  phone_number: "555-5555",
+  email: "email",
+  unique_id: 1234
+}];
+
 // Routes
 // =============================================================
 
@@ -37,8 +49,8 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/reserve", function(req, res) {
-  res.sendFile(path.join(__dirname, "reserve.html"));
+app.get("/reserved", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserved.html"));
 });
 
 app.get("/tables", function(req, res) {
@@ -62,21 +74,56 @@ app.get("/tables", function(req, res) {
 
 
 app.get("/api/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "apiTable.html"));
+
+  var chosen = req.params.reservation;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < reservation.length; i++) {
+      if (chosen === reservation[i].routeName) {
+        return res.json(reservation[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(reservation);
+
 });
 
 app.get("/api/waitlist", function(req, res) {
-  res.sendFile(path.join(__dirname, "waitlist.html"));
+  //res.sendFile(path.join(__dirname, "waitlist.html"));
+  var chosen = req.params.waitlist;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < waitlist.length; i++) {
+      if (chosen === waitlist[i].routeName) {
+        return res.json(waitlist[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(waitlist);
+
 });
 
 // Create New Characters - takes in JSON input
-app.post("/api/reserve", function(req, res) {
+app.post("/api/new", function(req, res) {
   var newReservation = req.body;
   newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
 
   console.log(newReservation);
 
-  reservation.push(newReservation);
+  if (reservation.length > 4){
+    waitlist.push(newReservation);
+    //window.alert("You've been added to the waitlist!");
+  }
+  else {
+    reservation.push(newReservation);
+    //window.alert("Your table is reserved!");
+  }
 
   res.json(newReservation);
 });
